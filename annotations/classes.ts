1dc8437925e6b2428358b91bef7a963ff9bfddcd
Selena Siri <selenasiri@gmail.com>
2020-12-24 15:16:41 -0500

    fields with classes and inheritance

--- annotations/classes.ts
+++ annotations/classes.ts
@@ -1,12 +1,19 @@
 class Vehicle { // Vehicle is the parent class
+  constructor( public color: string ) { }
+
   protected honk(): void {     // Class Method Modifiers: public and private
     console.log( 'beep' );
   }
 }
 
-const vehicle = new Vehicle();
+const vehicle = new Vehicle( 'orange' );
+console.log( vehicle.color )
 
 class Car extends Vehicle { // copy & paste methods from Vehicle to class Car
+  constructor( public wheels: number, color: string ) {
+    super( color );
+  }
+
   private drive(): void {   // private - used to restrict access to a function
     console.log( 'vroom' ); // redefines drive(); overrides the child class, returns 'vroom' instead of 'chugga chugga' 
   }
@@ -17,6 +24,6 @@ class Car extends Vehicle { // copy & paste methods from Vehicle to class Car
   }
 }
 
-const car = new Car();
+const car = new Car( 4, 'red' ); // When we call car here, it will automatically call the constructor at the top - which is why we need to define color as a string. Must now give (# of wheels, color as a string)
 car.startDrivingProcess();
 
