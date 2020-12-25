16cfde5b2d6a6d631a51131fb8d48ec3881db246
Selena Siri <selenasiri@gmail.com>
2020-12-25 09:44:07 -0500

    fields in classes

--- annotations/classes.ts
+++ annotations/classes.ts
@@ -1,5 +1,9 @@
 class Vehicle { // Vehicle is the parent class
-  constructor( public color: string ) { }
+  color: string = 'red';
+
+  constructor( color: string ) {
+    this.color = color;
+  }
 
   protected honk(): void {     // Class Method Modifiers: public and private
     console.log( 'beep' );
@@ -9,21 +13,21 @@ class Vehicle { // Vehicle is the parent class
 const vehicle = new Vehicle( 'orange' );
 console.log( vehicle.color )
 
-class Car extends Vehicle { // copy & paste methods from Vehicle to class Car
-  constructor( public wheels: number, color: string ) {
-    super( color );
-  }
+// class Car extends Vehicle { // copy & paste methods from Vehicle to class Car
+//   constructor( public wheels: number, color: string ) {
+//     super( color );
+//   }
 
-  private drive(): void {   // private - used to restrict access to a function
-    console.log( 'vroom' ); // redefines drive(); overrides the child class, returns 'vroom' instead of 'chugga chugga' 
-  }
+//   private drive(): void {   // private - used to restrict access to a function
+//     console.log( 'vroom' ); // redefines drive(); overrides the child class, returns 'vroom' instead of 'chugga chugga' 
+//   }
 
-  startDrivingProcess(): void {
-    this.drive();
-    this.honk();
-  }
-}
+//   startDrivingProcess(): void {
+//     this.drive();
+//     this.honk();
+//   }
+// }
 
-const car = new Car( 4, 'red' ); // When we call car here, it will automatically call the constructor at the top - which is why we need to define color as a string. Must now give (# of wheels, color as a string)
-car.startDrivingProcess();
+// const car = new Car( 4, 'red' ); // When we call car here, it will automatically call the constructor at the top - which is why we need to define color as a string. Must now give (# of wheels, color as a string)
+// car.startDrivingProcess();
 
